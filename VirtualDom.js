92bacd0772a3566b7fc58d3dcaf253a207a0c745
Bhairav <bhairavgotam066@gmail.com>
2023-11-14 21:54:59 +0530

    virtual DOM

---
+++ VirtualDom.js
@@ -0,0 +1,9 @@
+/*
+
+Virtual DOM lightwait copy of real DOM
+Virtual DOM is faster
+
+*/
+
+let x=4
+console.log(x)
--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,7 @@
 let arr = [3,5,2,5,2,3,5,8,6,3]
 
 for(let i=0; i<arr.length; i++){
-    for(let j=0; j<arr.length-i-1; j++){
+    for(let j=0; j<arr.length-1-i; j++){
         if(arr[j]>arr[j+1]){
             swap(arr,j,j+1)
         }
@@ -22,4 +22,4 @@ newArr.sort((a,b)=>{
     return 0
 })
 
-console.log(newArr)
+// console.log(newArr)
