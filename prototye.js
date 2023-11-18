39af2dc15e65ed76b4e612366ed0cd5e0ff026ec
Bhairav <bhairavgotam066@gmail.com>
2023-11-18 16:01:51 +0530

    added prototype and inheritance update

---
+++ prototye.js
@@ -0,0 +1,14 @@
+let name="bhairav"
+
+class Data{
+    constructor(name,age){
+        this.name=name,
+        this.age=age
+    }
+    type={
+        name:"gotam",
+        age:89
+    }
+}
+
+console.log(Data)
